2f64bbc63190431fc6ccced83e5fc503843a1edd
Sandeep Kumar <kkumar.sandeep89@gmail.com>
2014-12-27 01:48:56 +0530

    tar.gz was being treated as directory
    So, made changes to
    1. copy directory to location
    2. starting services
    3. run install script

--- install.sh
+++ install.sh
@@ -3,9 +3,19 @@ db_user="root"
 db_pass="redhat"
 db_name="fusion"
 
+#enter admin CP details here
+company_name="test"
+product_url="http://localhost/"
+first_name="admin"
+last_name="admin"
+username="admin"
+password="password"
+email="test@test.com"
+
 #extracting product files to document root
-tar -xzf product.tar.gz -C /usr/share/nginx/html/
-mv /usr/share/nginx/html/f*/upload/* /usr/share/nginx/html/.
+#tar -xzf product.tar.gz -C /usr/share/nginx/html/
+mv /product.tar.gz/*/upload/* /usr/share/nginx/html/.
+#mv /usr/share/nginx/html/f*/upload/* /usr/share/nginx/html/.
 chown -R www-data:www-data /usr/share/nginx/html/
 chmod -R 777 /usr/share/nginx/html/{__apps,__swift/files,__swift/cache,__swift/logs,__swift/geoip}
 cp /usr/share/nginx/html/__swift/config/config.php.new /usr/share/nginx/html/__swift/config/config.php
@@ -15,5 +25,14 @@ sed -i "s/'DB_USERNAME', 'root'/'DB_USERNAME', '$db_user'/p" /usr/share/nginx/ht
 sed -i "s/'DB_PASSWORD', ''/'DB_PASSWORD', '$db_pass'/" /usr/share/nginx/html/__swift/config/config.php
 sed -i "s/'DB_NAME', 'swift'/'DB_NAME', '$db_name'/" /usr/share/nginx/html/__swift/config/config.php
 
+#start mysql and nginx
+service mysql start
+service nginx start
+service php5-fpm start
+
 #creating database
 mysql -u$db_user -p$db_pass -e "create database $db_name default character set utf8 collate utf8_unicode_ci"
+
+#run setup script
+#Need data in format of "Company Name" "Product URL" "First Name" "Last Name" "Username" "Password" "Email" 
+php /usr/share/nginx/html/setup/console.setup.php 
